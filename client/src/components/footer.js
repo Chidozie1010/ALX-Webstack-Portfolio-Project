8971cba8d33d19141a21f930eebfe88bc623c545
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2024-01-08 20:22:58 +0100

    footer altered

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,11 +3,11 @@ import img from '../img/logo.png'
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="left">&copy; Breeltech. All rights reserved.</p>
+            <p className="right">Authored by <span>Jubreel, Oluwaseun & Chidozie</span></p>
             <div className='footer__logo'>
                 <img src={img} alt=""  />
             </div>
-            <p className="right">Authored by <span>Jubreel, Oluwaseun & Chidozie</span></p>
+            <p className="left">&copy; Breeltech. All rights reserved.</p>
         </footer>
     )
 }
