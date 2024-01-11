f0fcc670d2e066bc7f30ec61aeaf734cef4c5910
jubrealguy <whizkidjubrealguy@gmail.com>
2024-01-11 16:02:08 +0100

    worked on the header

--- client/src/pages/loginpage.js
+++ client/src/pages/loginpage.js
@@ -10,6 +10,8 @@ const Login = () => {
 
     const login = async (e) => {
         e.preventDefault()
+        setUsername("")
+        setPassword("")
         const response = await fetch('http://localhost:5000/login', {
             method: 'POST',
             body: JSON.stringify({username, password}),
